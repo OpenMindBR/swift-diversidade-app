050fcc561ab4b049cb9d7bcf1e7ac2236f356b03
Francisco José A. C. Souza <francisceioseph@gmail.com>
2016-03-08 16:24:56 -0300

    - Feature: Tela de About - Feature: Arquivos com nomes e links dos desenvolvedores - Feature: Listagem de Desenvolvedores em UITableView - Feature: Ao clicar na célula se abre o safari com o link do perfil do facebook do desenvolvedor/

--- Diversidade/AboutViewController.swift
+++ Diversidade/AboutViewController.swift
@@ -8,19 +8,74 @@
 
 import UIKit
 
-class AboutViewController: UIViewController {
+class AboutViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     @IBOutlet weak var menuItem: UIBarButtonItem!
-
+    @IBOutlet weak var tableView: UITableView!
+    
+    var datasource: NSArray?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        self.datasource = self.readDevelopersPlist()
         self.configureSideMenu(self.menuItem)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+        
+        if let datasource = self.datasource,
+            let developer = datasource[indexPath.row] as? NSDictionary,
+            let fbProfileLink  = developer["facebookLink"] as? String {
+                
+                self.openSafariAt(fbProfileLink)
+        }
+        
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier("about_cell")
+        
+        if let datasource = self.datasource,
+            let developer = datasource[indexPath.row] as? NSDictionary {
+                cell?.textLabel?.text = developer["name"] as? String
+        }
+        
+        return cell!
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        var rows = 0
+        
+        if let datasource = self.datasource {
+            rows = datasource.count
+        }
+        
+        return rows
+    }
+    
+    func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "Desenvolvedores"
+    }
+    func readDevelopersPlist() -> NSArray?{
+        let path = NSBundle.mainBundle().pathForResource("Developers", ofType: ".plist")
+        
+        return NSArray(contentsOfFile: path!)
+    }
+    
+    func openSafariAt(link:String){
+        let url = NSURL(string: link)
+        UIApplication.sharedApplication().openURL(url!)
     }
     
 
